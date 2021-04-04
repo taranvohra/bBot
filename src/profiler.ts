7dc24db8f934129c776afe01ec01619b828ada5c
taranvohra <taran.vohra@hotmail.com>
2021-04-04 18:25:45 +0530

    snapshot folder

--- src/profiler.ts
+++ src/profiler.ts
@@ -3,7 +3,7 @@ import heapdump from 'heapdump';
 const takeSnapshot = (): Promise<string | undefined> =>
   new Promise((rs, rj) => {
     heapdump.writeSnapshot(
-      `snapshots/${Date.now()}.heapsnapshot`,
+      `/tmp/bbot_snapshots/${Date.now()}.heapsnapshot`,
       (error, filename) => {
         if (error) rj(error);
         rs(filename);
