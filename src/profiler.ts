51747c0162389614e1d69eb6f80b49d917b2eac8
taranvohra <taran.vohra@hotmail.com>
2021-04-04 18:26:08 +0530

    fix writeSnapshot path

--- src/profiler.ts
+++ src/profiler.ts
@@ -3,7 +3,7 @@ import heapdump from 'heapdump';
 const takeSnapshot = (): Promise<string | undefined> =>
   new Promise((rs, rj) => {
     heapdump.writeSnapshot(
-      `/tmp/bbot_snapshots/${Date.now()}.heapsnapshot`,
+      `snapshots/${Date.now()}.heapsnapshot`,
       (error, filename) => {
         if (error) rj(error);
         rs(filename);
